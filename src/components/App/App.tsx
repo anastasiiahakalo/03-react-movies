14ecbdfa3147e1458f19e38f1b4ed67a1cef6db1
Anastasiia_Hakalo <anastasiiia.hakalo@gmail.com>
2026-06-03 11:49:06 -0600

    Add app component structure with state and API logic

---
+++ src/components/App/App.tsx
@@ -0,0 +1,74 @@
+
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] =
+    useState<Movie | null>(null);
+
+  const [loading, setLoading] =
+    useState(false);
+
+  const [error, setError] = useState(false);
+
+  
+const handleSearch = async (query: string) => {
+  try {
+    setMovies([]);
+    setError(false);
+    setLoading(true);
+
+    const data = await fetchMovies(query);
+
+    if (!data.length) {
+      toast.error("No movies found for your request.");
+      return;
+    }
+
+    setMovies(data);
+  } catch {
+    setError(true);
+    toast.error("Something went wrong. Try again.");
+  } finally {
+    setLoading(false);
+  }
+};
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+
+      <Toaster position="top-right" />
+
+      {loading && <Loader />}
+
+      {error && <ErrorMessage />}
+
+      {movies.length > 0 && (
+        <MovieGrid
+          movies={movies}
+          onSelect={setSelectedMovie}
+        />
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() =>
+            setSelectedMovie(null)
+          }
+        />
+      )}
+    </>
+  );
+}
