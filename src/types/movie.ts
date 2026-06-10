76c9236f89f49619e4629f0240624420c73f47c5
Anastasiia_Hakalo <anastasiiia.hakalo@gmail.com>
2026-06-09 20:57:21 -0600

    updated types and interface

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -6,4 +6,10 @@ export interface Movie {
   overview: string;
   release_date: string;
   vote_average: number;
-};
+}
+export interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
